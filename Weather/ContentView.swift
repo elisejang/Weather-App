a83c6466c127ae85b721b2e4f19b1b25556e2d0f
Elise Jang <elisej1@uci.edu>
2023-05-29 16:30:36 -0700

    main functionality down

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -6,17 +6,164 @@
 //
 
 import SwiftUI
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello")
-        }
-        .padding()
+protocol LocationUpdateProtocol {
+    func locationDidUpdate(to location: CLLocation)
+}
+
+struct ContentView: View , LocationUpdateProtocol{
+    func locationDidUpdate(to location: CLLocation) {
+        dLat = location.coordinate.latitude
+        dLon = location.coordinate.longitude
+        fetchData()
     }
+    
+    @State var locationName = "San Francisco"
+    @State var weatherImg = "01n"
+    @State var day = "Wednesday"
+    @State var desc = "Sunny"
+    @State var temp = "65"
+    
+    //
+    @State var DnN = "d"
+    let apikey = "bdf6d535e1951fa36662d87a91680a1f"
+    @State var dLat = 33.640495
+    @State var dLon = -117.844296
+    var actIndicator : NVActivityIndicatorView!
+    @State var userLocation: CLLocation?
+    var locationManagerclass = LocationManager()
+    
+
+    
+    
+    @State private var isLoading = true
+    
+    
+    var body: some View {
+        
+        VStack {
+            if isLoading {
+                ProgressView()
+            } else {
+                ZStack { //stacks along Z axis
+                        if DnN == "d"{
+                            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
+                                .ignoresSafeArea()
+
+                        }
+                        else{
+                            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                                .ignoresSafeArea()
+                        }
+                    
+                    VStack {
+                        Text(locationName)
+                            .font(.largeTitle)
+                        
+                            .foregroundColor(.white)
+                        
+                        Text(day)
+                            .font(.title2)
+                            .foregroundColor(Color.white)
+                        Image(weatherImg)
+                            .resizable()
+                            .frame(width: 300, height: 300)
+                        Text(desc)
+                            .font(.title2)
+                            .foregroundColor(Color.white)
+                        Text(temp)
+                            .font(.system(size: 100))
+                            .foregroundColor(Color.white)
+                        +
+                        Text("\u{2103}")
+                            .font(.system(size: 50))
+                            .baselineOffset(30.0)
+                            .foregroundColor(Color.white)
+                        Spacer()
+                        
+                    }
+                    .padding()
+                    
+                }
+            }
+        }
+        .onAppear{
+            fetchData()
+            
+        }
+    }
+    
+    func locationManager(_ userLocation: CLLocation?) {
+
+        self.dLat = locationManagerclass.userLocation?.coordinate.latitude ?? 33.640495
+        self.dLon = locationManagerclass.userLocation?.coordinate.longitude ?? 117.844296
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(dLat)&lon=\(dLon)&appid=\(apikey)&units=metric").responseJSON {response in
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                
+                self.locationName = jsonResponse["name"].stringValue
+                self.weatherImg = iconName
+                self.desc = jsonWeather["main"].stringValue
+                self.temp = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEEE"
+                self.day = dateFormatter.string(from: date)
+                
+                let suffix = iconName.suffix(1)
+                if(suffix == "n"){
+                    self.DnN = "n"
+                }else{
+                    self.DnN = "d"
+                }
+            }
+        }
+    }
+    
+    
+    
+    func fetchData() {
+        isLoading = true
+        
+        locationManagerclass.delegate = self
+        locationManagerclass.startUpdatingLocation()
+        locationManager(userLocation)
+        isLoading = false
+        
+        }
+    
+
+    class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+        private let locationManager = CLLocationManager()
+        @Published var userLocation: CLLocation?
+        var delegate: LocationUpdateProtocol?
+
+        override init() {
+            super.init()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+        }
+
+        func startUpdatingLocation() {
+            locationManager.startUpdatingLocation()
+        }
+
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.last {
+                userLocation = location
+                delegate?.locationDidUpdate(to: location)
+            }
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
--- Weather/WeatherApp.swift
+++ Weather/WeatherApp.swift
@@ -6,9 +6,40 @@
 //
 
 import SwiftUI
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
 
 @main
-struct WeatherApp: App {
+struct WeatherApp: App{
+//    let apikey = "bdf6d535e1951fa36662d87a91680a1f"
+//    var dLat = 33.6424
+//    var dLon = 117.8417
+//    var actIndicator : NVActivityIndicatorView!
+//    let locManager = CLLocationManager()
+//    let locManagerDelegate = LocationManagerDelegate()
+//
+//    init() {
+//        GeometryReader { geometry in
+//            let indicatorSize: CGFloat = 70
+//            let indicatorFrame = CGRect(x: (geometry.size.width-indicatorSize)/2, y: (geometry.size.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+//            actIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+//            actIndicator.backgroundColor = UIColor.black
+//            view.addSubview(actIndicator)
+//
+//
+//
+//            actIndicator.startAnimating()
+//            locManager.requestWhenInUseAuthorization()
+//
+//            if (CLLocationManager.locationServicesEnabled()){
+//                locManager.delegate = locManagerDelegate
+//                locManager.desiredAccuracy = kCLLocationAccuracyBest
+//                locManager.startUpdatingLocation()
+//            }
+//        }
+//        }
     var body: some Scene {
         WindowGroup {
             ContentView()
